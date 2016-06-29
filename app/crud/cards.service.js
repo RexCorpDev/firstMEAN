6f07bc88ee09c44976cbac44b5324b63373a424b
RexCorpDev <rexcorpfx@gmail.com>
2016-06-28 22:35:04 -0700

    reformat services.js

--- app/crud/cards.service.js
+++ app/crud/cards.service.js
@@ -3,7 +3,7 @@
 angular.module('flashCardApp.crudModule')
 .service('Card', function($http){
   this.getCards     = ()          => $http.get    ('/api/crud/cards');
-  this.getCardById  = id          => $http.get    ('api/crud/cards/', id);
+  this.getCardById  = id          => $http.get    ('api/crud/cards', id);
   this.create       = newCard     => $http.post   ('/api/crud/cards', newCard);
   this.edit         = editedCard  => $http.put    (`/api/crud/cards/${editedCard._id}`, editedCard);
   this.delete       = id          => $http.delete (`/api/crud/cards/${id}`);
